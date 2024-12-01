28d629eaa3fe688450887bd075088c08c7443068
ashir174 <ashirsial174@gmail.com>
2024-12-01 17:11:03 +0500

    Changing Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:androidstudioprojects/MyHomePage.dart';
 import 'package:flutter/material.dart';
-import 'SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SplashScreen(),
+      home: const MyHomePage(title: "Dashboard"),
     );
   }
 }
