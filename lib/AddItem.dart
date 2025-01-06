88db29ba6ed88ec79c7e56ddeabba0446a330cad
ashir174 <ashirsial174@gmail.com>
2025-01-06 16:57:18 +0500

    Updated

--- lib/AddItem.dart
+++ lib/AddItem.dart
@@ -12,8 +12,13 @@ class AddItem extends StatefulWidget {
 
 class _AddItemState extends State<AddItem> {
   final TextEditingController _titleController = TextEditingController();
+  final FocusNode _textFieldFocusNode = FocusNode();
   DateTime _selectedDate = DateTime.now();
   TimeOfDay _selectedTime = TimeOfDay.now();
+
+  void _dismissKeyboard() {
+    FocusScope.of(context).unfocus();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,74 +26,85 @@ class _AddItemState extends State<AddItem> {
       appBar: AppBar(
         title: const Text("Add Task"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(
+      body: GestureDetector(
+        onTap: _dismissKeyboard, // Dismiss the keyboard on tap outside
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                focusNode: _textFieldFocusNode,
+                decoration: const InputDecoration(
                   labelText: 'Task Title',
-                  labelStyle:
-                      TextStyle(color: Color.fromARGB(255, 124, 124, 124))),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Text('Date: ${DateFormat('dd-MM-yyyy').format(_selectedDate)}'),
-                IconButton(
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: () async {
-                    DateTime? selected = await showDatePicker(
-                      context: context,
-                      initialDate: _selectedDate,
-                      firstDate: DateTime(2000),
-                      lastDate: DateTime(2100),
-                    );
-                    if (selected != null && selected != _selectedDate) {
-                      setState(() {
-                        _selectedDate = selected;
-                      });
-                    }
-                  },
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 124, 124, 124),
+                  ),
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Time: ${_selectedTime.format(context)}'),
-                IconButton(
-                  icon: const Icon(Icons.access_time),
-                  onPressed: () async {
-                    TimeOfDay? selected = await showTimePicker(
-                      context: context,
-                      initialTime: _selectedTime,
-                    );
-                    if (selected != null && selected != _selectedTime) {
-                      setState(() {
-                        _selectedTime = selected;
-                      });
-                    }
-                  },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    'Date: ${DateFormat('dd-MM-yyyy').format(_selectedDate)}',
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? selected = await showDatePicker(
+                        context: context,
+                        initialDate: _selectedDate,
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      );
+                      if (selected != null && selected != _selectedDate) {
+                        setState(() {
+                          _selectedDate = selected;
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('Time: ${_selectedTime.format(context)}'),
+                  IconButton(
+                    icon: const Icon(Icons.access_time),
+                    onPressed: () async {
+                      TimeOfDay? selected = await showTimePicker(
+                        context: context,
+                        initialTime: _selectedTime,
+                      );
+                      if (selected != null && selected != _selectedTime) {
+                        setState(() {
+                          _selectedTime = selected;
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  DateTime finalDateTime = DateTime(
+                    _selectedDate.year,
+                    _selectedDate.month,
+                    _selectedDate.day,
+                    _selectedTime.hour,
+                    _selectedTime.minute,
+                  );
+                  widget.onAdd(_titleController.text, finalDateTime);
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: const Color.fromARGB(255, 185, 117, 97),
                 ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                DateTime finalDateTime = DateTime(
-                  _selectedDate.year,
-                  _selectedDate.month,
-                  _selectedDate.day,
-                  _selectedTime.hour,
-                  _selectedTime.minute,
-                );
-                widget.onAdd(_titleController.text, finalDateTime);
-                Navigator.pop(context);
-              },
-              child: const Text("Add Task"),
-            ),
-          ],
+                child: const Text("Add Task"),
+              ),
+            ],
+          ),
         ),
       ),
     );
